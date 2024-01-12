5a9a805c1c4f6dfe1179583d28411749b06f3844
DarioLoForti <lofortidario@gmail.com>
2024-01-12 12:46:08 +0100

    mileston 0 completed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script>
+import { store } from './store.js'
 export default {
+  data(){
+    return{
+      store
+    }
+  }
   
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,7 @@
 import{ reactive } from 'vue';
 export const store = reactive({
     UrlPointMovie: 'https://api.themoviedb.org/3/search/movie?',
-
     UrlPointSeries: 'https://api.themoviedb.org/3/search/tv?',
-    
-    keyApi: 'ebb547ab2123b8e09060d57aad2efea6',
-    
+    keyApi: 'ebb547ab2123b8e09060d57aad2efea6', 
 })
 
