f49ee86da73223b7dd54aa3fa2d619e89d20ebe3
DarioLoForti <lofortidario@gmail.com>
2024-01-12 12:36:14 +0100

    adding store.js

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import{ reactive } from 'vue';
+export const store = reactive({
+    UrlPointMovie: 'https://api.themoviedb.org/3/search/movie?',
+
+    UrlPointSeries: 'https://api.themoviedb.org/3/search/tv?',
+    
+    keyApi: 'ebb547ab2123b8e09060d57aad2efea6',
+    
+})
+
