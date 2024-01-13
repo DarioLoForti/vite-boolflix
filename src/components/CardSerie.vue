5805b5634f51b32b897228ba1a5873126350a7e4
DarioLoForti <lofortidario@gmail.com>
2024-01-13 22:43:35 +0100

    adding flip card

--- src/components/CardSerie.vue
+++ src/components/CardSerie.vue
@@ -35,29 +35,74 @@ export default {
 }
 </script>
 <template lang="">
-    <div class="my-card">
-        
-            <img class="posterImg" :src="`https://image.tmdb.org/t/p/w342/${card.poster_path}`" alt="">
-            <h5> Titolo: {{ card.name }}</h5>
-            <h6> Titolo Originale: {{ card.original_name }}</h6>
-            <div class="lingua"> <h6>Lingua: {{ card.original_language.toUpperCase()}}</h6> <img class="flag" :src="flag(card.original_language)">
+    <div class="my-card my-2 px-2">
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img class="posterImg" :src="`https://image.tmdb.org/t/p/w342/${card.poster_path}`" alt="">
+                </div>
+                <div class="flip-card-back">
+                    <h5> Titolo: {{ card.name }}</h5>
+                    <h6> Titolo Originale: {{ card.original_name }}</h6>
+                    <div class="lingua"> <h6>Lingua: {{ card.original_language.toUpperCase()}}</h6> <img class="flag" :src="flag(card.original_language)">
+                    </div>
+                    <div class="voto"> <h6>Voto: {{ card.vote_average }}</h6> </div>
+                </div>
             </div>
-            <div class="voto"> <h6>Voto: {{ card.vote_average }}</h6> </div>
-        
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
 .my-card{
-    width: calc(100% / 4 - 20px);
+    width: calc(100% / 5 - 20px);
     margin: 10px;
    }
    .posterImg{
-       width: 100%;
-       margin-bottom: 30px;
-}
+    height: 100%;
+    width: 100%;
+    
+   }
+   .flip-card {
+    background-color: transparent;
+    width: 100%;
+    height: 300px;
+    perspective: 1000px; 
+  }
+ 
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+  }
+  
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+  
+  .flip-card-front, .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+  }
+  
+  .flip-card-front {
+    background-color: #bbb;
+    color: black;
+  }
+  
+  .flip-card-back {
+    background-color: black;
+    color: white;
+    transform: rotateY(180deg);
+  }
 
 h5,
 h6{
+    margin-top: 20px;
     color: white;
 }
 
